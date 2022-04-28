9de2d99a9ec7d520dd8b681d5eb5a9db2f289251
Anthonyrgn <anthony.ringeisen.dev@gmail.com>
2022-04-28 19:52:44 +0200

    button connexion

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttersocial/util/images.dart';
 
 class AuthController extends StatefulWidget {
 
@@ -7,12 +8,81 @@ class AuthController extends StatefulWidget {
 }
 
 class AuthState extends State<AuthController>{
+
+  PageController? _pageController;
+  TextEditingController? _mail;
+  TextEditingController? _password;
+  TextEditingController? _name;
+  TextEditingController? _surname;
+
+  @override
+  void initState() {
+    _pageController = PageController();
+    _mail = TextEditingController();
+    _password = TextEditingController();
+    _name = TextEditingController();
+    _surname = TextEditingController();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController?.dispose();
+    _mail?.dispose();
+    _password?.dispose();
+    _name?.dispose();
+    _surname?.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: Center(
-        child: Text("Ici on fera notre Auth"),
+      body: SingleChildScrollView(
+        child: InkWell(
+          onTap: null,
+          child: SafeArea(
+            child: Column(
+              children: [
+                Image.asset(logoImage, height: MediaQuery.of(context).size.height / 5),
+              ],
+            ),
+          )
+
+        ),
+      )
+    );
+  }
+
+  Widget logOrCreateButtons(){
+    return Container(
+      width: 300,
+      height: 50,
+      child: CustomPaint(
+        painter: ,
+        child: Row(
+          btn("Se connecter"),
+          btn("Créer un compte")
+        ),
+      ),
+    );
+  }
+
+  Expanded btn(String name){
+    return Expanded(
+      child: TextButton(
+        child: Text(name),
+        onPressed: (){
+          int page = (_pageController?.page == 0.0) ? 1 : 0;
+          _pageController?.animateToPage(
+              page,
+              duration: Duration(milliseconds: 500),
+              curve: Curves.bounceOut
+          );
+        },
       ),
     );
   }
